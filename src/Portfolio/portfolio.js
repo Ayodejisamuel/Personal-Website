71ace2e3a47b7dce2e6de82e584975ba8827cd11
Ayodeji <ayodejisamuel80@gmail.com>
2024-08-04 10:50:56 +0100

    additional changes

--- src/Portfolio/portfolio.js
+++ src/Portfolio/portfolio.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import  './portfolio.css'
 import Ayodeji from '../Images/Ayodeji.jpg'
+import weatherapp from '../Images/weatherapp.png'
+import movieapp from '../Images/movieapp.png'
+import swissmark from '../Images/swissmark.png'
+import academyui from '../Images/academyui.png'
+import todoapp from '../Images/todo-app (1).png'
  
 
 
@@ -12,11 +17,11 @@ const Portfolio = ()  => {
              <h1 className="contactdesc">Portfolio</h1>
             <h3 className="portinfo">Kindly find below my Rescent Projects built by utilizing amazing APis, html, css and Javascript using React as a frame work. I am commmited to bring my skills and experience to help grow buisinesses and create strong online presence.</h3>
             <div className='works'>
-                < a href='' target='_blank' ><img src={Ayodeji}  className='worksimg'/></a>
-                < a href='' target='_blank' > <img src={Ayodeji} className='worksimg'/></a>
-                < a href='' target='_blank' ><img src={Ayodeji} className='worksimg'/></a>
-                < a href='' target='_blank' ><img src={Ayodeji} className='worksimg'/></a>
-                < a href='' target='_blank' ><img src={Ayodeji} className='worksimg'/></a>
+                < a href='https://ayodejisamuel.github.io/Swissmark/' target='_blank' ><img src={swissmark}  className='worksimg'/></a>
+                < a href='https://ayodejisamuel.github.io/react-weather-app/' target='_blank' > <img src={weatherapp} className='worksimg'/></a>
+                < a href='https://ayodejisamuel.github.io/Movie-App/' target='_blank' ><img src={movieapp} className='worksimg'/></a>
+                < a href='' target='_blank' ><img src={todoapp} className='worksimg'/></a>
+                < a href='https://ayodejisamuel.github.io/Academy-UI/' target='_blank' ><img src={academyui} className='worksimg'/></a>
                 < a href='' target='_blank' ><img src={Ayodeji} className='worksimg'/></a>
             </div>
            
--- src/Skills/skills.js
+++ src/Skills/skills.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './skills.css';
 import  Ayodeji from '../Images/Ayodeji.jpg';
+import academyui from '../Images/academyui.png'
 import Card from '../Card/Card';
 
 const Skills = () => {
@@ -9,21 +10,21 @@ const Skills = () => {
  <section id='skill'>
 
 <h1 className="contactdesc">What I Do</h1>
-<h3 className="portinfo">I am a skilled and passionate front-end engineer with experience in creating visualling appealing and user-friendly website. I have a strong understanding on design and a keen eye on details. I am proficient in HTML Css and JavaScript as well as Javascript Frameworks and CSS processors.</h3>
+<h3 className="portinfo">I am a skilled and passionate front-end engineer with experience in creating visualling appealing and user-friendly website. I have a strong understanding on design and a keen eye on details. I am proficient in HTML CSS and JavaScript, React, NextJs as well as Javascript Frameworks and CSS pre-processors.</h3>
 
 <div className='skillBars'>
     {/* <Card  img={Ayodeji} name="ReactJS" description="Javascript framework library for creating reuseable user-interfaces."/> */}
 
-    <Card  img={Ayodeji} name="Responsive Design" description="UI/UX design this is the demo text"/>
-    <Card  img={Ayodeji} name="Responsive Design" description="UI/UX design this is the demo text"/>
-    <Card  img={Ayodeji} name="Responsive Design" description="UI/UX design this is the demo text"/>
-    <Card  img={Ayodeji} name="Responsive Design" description="UI/UX design this is the demo text"/>
+    <Card  img={Ayodeji} name="Responsive Design" description="SwissMark"/>
+    <Card  img={academyui} name="UI/UX Design" description="Academy UI"/>
+    <Card  img={Ayodeji} name="API Integration" description="Movie App"/>
+    <Card  img={Ayodeji} name="Git/Version Control" description="Weather App"/>
 
-    <Card  img={Ayodeji} name="UI/UX Design" description="ui/ux design this is the demo text"/>
+    <Card  img={Ayodeji} name="State Management" description="Academy UI"/>
 
-    <Card  img={Ayodeji} name="API Integration" description="ui/ux design this is the demo text"/>
+    <Card  img={Ayodeji} name="JavaScript" description="Todo-App"/>
 
-    <Card  img={Ayodeji} name="Git/Version Control" description="ui/ux design this is the demo text"/>
+    <Card  img={Ayodeji} name="JavaScript" description="Quiz App"/>
 
 </div>
 </section>
