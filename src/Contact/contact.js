a2cc6b2073310cda7ee2a0a55b61220815440005
Ayodeji <ayodejisamuel80@gmail.com>
2024-08-09 20:32:08 +0100

    new changes

--- src/Contact/contact.js
+++ src/Contact/contact.js
@@ -2,7 +2,8 @@ import React, { useEffect, useRef } from "react";
 import "./contact.css";
 import emailjs from '@emailjs/browser';
 import ScrollReveal from "scrollreveal";
-import 'boxicons/css/boxicons.min.css'; 
+import 'boxicons/css/boxicons.min.css';
+
 
 const Contact = () => {
   const form = useRef();
@@ -39,12 +40,25 @@ const Contact = () => {
           <input type="email" name="from_email" className="email" placeholder="Your Email" required />
           <textarea name="message" className="msg" placeholder="Your Message" rows="5" required></textarea><br />
           <button type="submit" value="Send" className="submitbtn">Submit</button>
-        </form>
-        <div className="linkcontainer">
-        <a href="https://wa.link/h0nwi2" target="_blank"><box-icon className='whatsapp' type="logo" name="whatsapp"></box-icon></a>
-          <a href="https://github.com/Ayodejisamuel/" target="_blank"><box-icon type="logo" name="github"></box-icon></a>
-          <a href="https://www.linkedin.com/in/ayodeji-faleye-0b683016a/" target="_blank"><box-icon type="logo" name="linkedin-square"></box-icon></a>
+
+          <div className="linkcontainer">
+          <a href="tel:+2349038283807" target="_blank" rel="noopener noreferrer">
+            <i className='bx bx-phone phone'></i>
+          </a>
+          <a href="https://wa.link/h0nwi2" target="_blank" rel="noopener noreferrer">
+            <i className='bx bxl-whatsapp whatsapp'></i>
+          </a>
+          <a href="https://github.com/Ayodejisamuel/" target="_blank" rel="noopener noreferrer">
+            <i className='bx bxl-github'></i>
+          </a>
+          <a href="https://www.linkedin.com/in/ayodeji-faleye-0b683016a/" target="_blank" rel="noopener noreferrer">
+            <i className='bx bxl-linkedin-square'></i>
+          </a>
         </div>
+        </form>
+
+        
+
       </div>
     </section>
   );
