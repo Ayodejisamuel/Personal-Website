4d60dde88daeda8971df94846dd365747ee94c33
Ayodeji <ayodejisamuel80@gmail.com>
2024-08-10 11:44:02 +0100

    new changes

--- src/Intro/intro.js
+++ src/Intro/intro.js
@@ -3,7 +3,7 @@ import "./intro.css";
 import deji from "../Images/deji.png";
 import Typed from "typed.js";
 import ScrollReveal from "scrollreveal";
- 
+
 
 const Intro = () => {
   useEffect(() => {
@@ -40,7 +40,7 @@ const Intro = () => {
       origin: "left",
     });
     ScrollReveal().reveal(".introparag", {
-    //   reset: true,
+      //   reset: true,
       distance: "80px",
       duration: 2000,
       delay: 500,
@@ -56,7 +56,7 @@ const Intro = () => {
     <section className="intro" id="intro">
       <div className="intro-container">
         <div className="text-content">
-          <h3 className="hello">Hello, It's me</h3>
+          <span className="hello">Hello, It's me</span>
           <h1 className="Iam">
             Ayodeji Samuel<span className="name"></span>
           </h1>
@@ -68,6 +68,20 @@ const Intro = () => {
               user-friendly websites.
             </p>
           </strong>
+          <div className="linkcontainer">
+            <a href="tel:+2349038283807" target="_blank" rel="noopener noreferrer">
+              <i className='bx bx-phone phone'></i>
+            </a>
+            <a href="https://wa.link/h0nwi2" target="_blank" rel="noopener noreferrer">
+              <i className='bx bxl-whatsapp whatsapp'></i>
+            </a>
+            <a href="https://github.com/Ayodejisamuel/" target="_blank" rel="noopener noreferrer">
+              <i className='bx bxl-github'></i>
+            </a>
+            <a href="https://www.linkedin.com/in/ayodeji-faleye-0b683016a/" target="_blank" rel="noopener noreferrer">
+              <i className='bx bxl-linkedin-square'></i>
+            </a>
+          </div>
         </div>
         <div className="image-container">
           <img className="" src={deji} alt="BackgroundImage" />
